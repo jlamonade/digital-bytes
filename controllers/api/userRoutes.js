19eb6250149c9f44dbdf24ac286dd2ccba315b15
Jason Lam <nyjasonlam@gmail.com>
2021-05-23 15:34:45 -0400

    Added create user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 const { User, BlogPost } = require("../../models");
 
+// CREATE
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    if (userData) res.status(200).json("User successfully created.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// READ
 router.get("/", async (req, res) => {
   try {
     const userData = await User.findAll({
-      include: BlogPost
+      include: BlogPost,
     });
     res.status(200).json(userData);
   } catch (err) {
@@ -19,12 +30,17 @@ router.get("/:id", async (req, res) => {
         id: req.params.id,
       },
       include: BlogPost,
-    })
+    });
     if (userData) res.status(200).json(userData);
     else res.status(404).json("404: User Data Not Found.");
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-}) 
+});
+
+// UPDATE
+// router.put();
+
+// DELETE
 
 module.exports = router;
