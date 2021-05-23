64f4a2ad8b74c987c7ac32b8b8788c74005f18ec
Jason Lam <nyjasonlam@gmail.com>
2021-05-23 13:30:44 -0400

    Added seeds/index.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,17 @@
-const seedUserData =
+const seedPostData = require("./seedPostData");
+const seedUserData = require("./seedUserData");
 
 const sequelize = require("../config/connection");
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+  await seedUserData();
+  console.log("\n----- USERS SYNCED -----\n");
+  await seedPostData();
+  console.log("\n----- POSTS SYNCED -----\n");
+
+  process.exit();
+};
+
+seedAll();
