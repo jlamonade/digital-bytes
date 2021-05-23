c95421c3d8c00b05670f948b3b8baeac654bdba6
Jason Lam <nyjasonlam@gmail.com>
2021-05-23 15:24:56 -0400

    Fixed get all users route, now correctly includes all associated blog posts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,6 @@ const userRoutes = require("./userRoutes");
 const blogPostRoutes = require("./blogPostRoutes");
 
 router.use("/users", userRoutes);
-router.use("/posts", blogPostRoutes);
+// router.use("/posts", blogPostRoutes);
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,10 +4,12 @@ const { User, BlogPost } = require("../../models");
 router.get("/", async (req, res) => {
   try {
     const userData = await User.findAll({
-      include: [{BlogPost}],
+      include: BlogPost
     });
     res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+module.exports = router;
