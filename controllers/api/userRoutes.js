ce675efdd649afa3b1f9d816696cb269671ba6fe
Jason Lam <nyjasonlam@gmail.com>
2021-05-23 15:27:56 -0400

    Added get user by id route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,4 +12,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      where: {
+        id: req.params.id,
+      },
+      include: BlogPost,
+    })
+    if (userData) res.status(200).json(userData);
+    else res.status(404).json("404: User Data Not Found.");
+  } catch(err) {
+    res.status(500).json(err);
+  }
+}) 
+
 module.exports = router;
