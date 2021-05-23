7549622bad2c991c183b1ab2631806c4c0c1133b
Jason <nyjasonlam@gmail.com>
2021-05-23 02:15:11 -0400

    Added BlogPost model

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,2 +1,37 @@
 const { Model, DataTypes } = require("sequelize");
 
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog_post",
+  }
+);
