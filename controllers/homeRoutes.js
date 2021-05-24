9a89174b895895615712e7469460d02a9955205e
Jason Lam <nyjasonlam@gmail.com>
2021-05-23 21:45:56 -0400

    added .gitattributes to local env

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,7 +7,7 @@ router.get("/login", async (req, res) => {
   return await res.render("login");
 });
 router.get("/newpost", async (req, res) => {
-    return await res.render("newPost");
-})
+  return await res.render("newPost");
+});
 
 module.exports = router;
