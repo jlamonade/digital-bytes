4f1cbbe55c48408ea6a252147f3ae5176f9f23d4
Jason Lam <nyjasonlam@gmail.com>
2021-05-24 20:10:29 -0400

    moved index rendering logic to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,19 @@
 const router = require('express').Router()
+const { BlogPost, User } = require('../models')
 
 router.get('/', async (req, res) => {
-  await res.render('index')
+  try {
+    const blogData = await BlogPost.findAll({
+      include: { model: User, attributes: ['name'] }
+    })
+    const posts = blogData.map(element => element.get({ plain: true }))
+    if (blogData) res.render('index', { posts: posts })
+    else res.status(404).json('404 Blog Data Not Found.')
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
+
 router.get('/login', async (req, res) => {
   return await res.render('login')
 })
