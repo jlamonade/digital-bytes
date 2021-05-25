2b2aca06c01b3e20b99264be6928593efbf65b88
Jason Lam <nyjasonlam@gmail.com>
2021-05-24 20:46:53 -0400

    Added login and signup forms and templates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,6 +17,9 @@ router.get('/', async (req, res) => {
 router.get('/login', async (req, res) => {
   return await res.render('login')
 })
+router.get('/signup', async (req, res) => {
+  return await res.render('signup')
+})
 router.get('/dashboard', async (req, res) => {
   return await res.render('dashboard')
 })
