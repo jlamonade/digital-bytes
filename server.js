e0b05417919e550aa3611a99a4e1519ae3138746
Jason Lam <nyjasonlam@gmail.com>
2021-05-25 17:21:06 -0400

    Added jaws db sequelize connection prefs

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-const express = require("express");
-const routes = require("./controllers");
-const exphbs = require("express-handlebars");
-const sequelize = require("./config/connection");
-const path = require("path");
+const express = require('express')
+const routes = require('./controllers')
+const exphbs = require('express-handlebars')
+const sequelize = require('./config/connection')
+const path = require('path')
 
-const app = express();
-const hbs = exphbs.create({});
-const PORT = process.env.PORT || 3001;
+const app = express()
+const hbs = exphbs.create({})
+const PORT = process.env.PORT || 3001
 
 // HANDLEBARS
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 // MIDDLEWARE
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // CONTROLLERS
-app.use(routes);
+app.use(routes)
 
 // INIT
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}.`);
-  });
-});
-
+    console.log(`App listening on port ${PORT}.`)
+  })
+})
