a519bdedc93fcfc52714dcf95d4ff79acabe5038
Jason Lam <nyjasonlam@gmail.com>
2021-05-25 09:45:30 -0400

    Corrected format for createPost.js

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -1,22 +1,22 @@
 const handleCreatePost = async (e) => {
-  e.preventDefault();
-  const title = document.querySelector("#input-title").value;
-  const body = document.querySelector("#input-body").value;
+  e.preventDefault()
+  const title = document.querySelector('#input-title').value
+  const body = document.querySelector('#input-body').value
 
-  await fetch("/api/posts", {
+  await fetch('/api/posts', {
     method: 'POST',
     body: {
-      "title": title,
-      "body": body,
-      "user_id": 1
+      title: title,
+      body: body,
+      user_id: 1
       // TODO: fix body to pass user_id properly
     },
     header: {
-      "Content-type": "application/json",
-    },
-  });
-};
+      'Content-type': 'application/json'
+    }
+  })
+}
 
 document
-  .querySelector("#new-post-form")
-  .addEventListener("submit", handleCreatePost);
+  .querySelector('#new-post-form')
+  .addEventListener('submit', handleCreatePost)
