f6d7761d1404011f426a2f0c67cae4f38cdffe2f
Jason <nyjasonlam@gmail.com>
2021-05-25 22:47:40 -0400

    added helpers.js

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const session = require('express-session')
 const sequelize = require('./config/connection')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const path = require('path')
+const helpers = require('./utils/helpers')
 
 const app = express()
-const hbs = exphbs.create({})
+const hbs = exphbs.create({ helpers })
 const PORT = process.env.PORT || 3001
 
 // HANDLEBARS
