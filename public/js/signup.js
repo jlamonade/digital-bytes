30853be2ca6ee3f7bd73f2a6a12ff8ae8b418dc6
Jason <nyjasonlam@gmail.com>
2021-05-26 23:05:20 -0400

    commente signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,6 +5,7 @@ const signUpHandler = async (e) => {
   const password = document.querySelector('#input-password').value.trim()
 
   if (name && email && password) {
+    // user info is sent through the request body
     const response = await fetch('/api/users/signup', {
       method: 'POST',
       body: JSON.stringify({
@@ -16,7 +17,7 @@ const signUpHandler = async (e) => {
         'Content-type': 'application/json'
       }
     })
-    if (response.ok) {
+    if (response.ok) { // if user successfully signs up send them to login page
       document.location.replace('/login')
     } else {
       alert('Please correct information.')
