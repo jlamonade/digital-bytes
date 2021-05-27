e673e75d9af3e7d2632d03f647943cc1161d41d0
Jason Lam <nyjasonlam@gmail.com>
2021-05-26 21:28:46 -0400

    put new comment route behind loggedin validation

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,36 +2,20 @@ const router = require('express').Router()
 const { Comment } = require('../../models')
 
 router.post('/new', async (req, res) => {
-  console.log(req)
-  try {
-    const commentData = await Comment.create({
-      body: req.body.body,
-      post_id: req.body.postId,
-      user_id: req.session.user_id
-    })
-    if (commentData) {
-      res.status(200).json(commentData)
+  if (req.session.loggedIn) {
+    try {
+      const commentData = await Comment.create({
+        body: req.body.body,
+        post_id: req.body.postId,
+        user_id: req.session.user_id
+      })
+      if (commentData) {
+        res.status(200).json(commentData)
+      }
+    } catch (err) {
+      res.status(500).json('500 Internal Server Error.')
     }
-  } catch (err) {
-    res.status(500).json('500 Internal Server Error.')
   }
 })
 
 module.exports = router
-
-// router.get('/comment', async (req, res) => {
-//   try {
-//     const commentData = await Comment.findAll({
-//       where: {
-//         post_id: req.body.post_id
-//       }
-//     })
-//     if (commentData) {
-//       res.json(commentData)
-//     } else {
-//       res.status(404).json('404 comment not found.')
-//     }
-//   } catch (err) {
-//     res.status(500).json(err)
-//   }
-// })
