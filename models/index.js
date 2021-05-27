13be73324f878594b97f3853c2b66feaea574b22
Jason <nyjasonlam@gmail.com>
2021-05-26 22:38:01 -0400

    Commented models/index.js

--- models/index.js
+++ models/index.js
@@ -2,23 +2,28 @@ const User = require('./User')
 const BlogPost = require('./BlogPost')
 const Comment = require('./Comment')
 
+// one user to each blog post
 BlogPost.belongsTo(User, {
   foreignKey: 'user_id'
 })
 
+// blog post can have many comments
 BlogPost.hasMany(Comment, {
   foreignKey: 'post_id',
   onDelete: 'CASCADE'
 })
 
+// Comment belongs to both BlogPost and User
 Comment.belongsTo(BlogPost, {
   foreignKey: 'post_id'
 })
 
+// Comment has one owner
 Comment.belongsTo(User, {
   foreignKey: 'user_id'
 })
 
+// Users have many blog posts
 User.hasMany(BlogPost, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
